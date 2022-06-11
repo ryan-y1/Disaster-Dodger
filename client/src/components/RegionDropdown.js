488a6bf00616f692bc29889b934c97a671c69885
Cookie <hanlicookie@outlook.com>
2022-06-11 09:26:53 -0700

    update form
    rounded corners
    background color
    added all states to dropdown menu
    padding and margin

--- client/src/components/RegionDropdown.js
+++ client/src/components/RegionDropdown.js
@@ -5,6 +5,59 @@ import { MenuItem } from '@mui/material'
 function RegionDropdown() {
     const [region, setRegion] = React.useState('')
 
+    const USStateList = [
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming"
+    ]
+
     const handleChange = (event) => {
         setRegion(event.target.value)
     }
@@ -13,11 +66,14 @@ function RegionDropdown() {
         <Select 
             value={region}
             onChange={handleChange}
+            color="primary"
+            sx={{
+                width: "300px"
+            }}
         >
-            <MenuItem value={"a"}>a</MenuItem>
-            <MenuItem value={"b"}>b</MenuItem>
-            <MenuItem value={"c"}>c</MenuItem>
-            <MenuItem value={"d"}>d</MenuItem>
+            {USStateList.map(e => {
+                return <MenuItem value={e}>{e}</MenuItem>
+            })}
         </Select>
     )
 }
