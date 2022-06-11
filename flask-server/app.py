85a116f455895fe90060c66a6fd027278001f648
Ryan Yi <63077981+ryan-y1@users.noreply.github.com>
2022-06-11 16:11:47 -0700

    Update app.py
    Added process_json function to /searchRegion route

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Bonjour Tout Le Monde"
+
+@app.route("/searchRegion", methods={"Post"})
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
