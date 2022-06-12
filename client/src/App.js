1f4851b28d0bb9f728c3834cc8b90c12706ec3bc
Cookie <hanlicookie@outlook.com>
2022-06-12 03:22:22 -0700

    about page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import './App.css';
 
@@ -59,6 +59,22 @@ function App() {
           </ParallaxLayer>
         </div>
       </Parallax>
+      <div className='about'>
+        <div>
+          <div>
+            <Typography variant='h2'>About</Typography>
+            <Typography variant='p'>
+              Disaster Dodger is a website built to assist in safer evacuation in the case of a potential natural disaster. Due to changes in climate conditions, there has been an overall increase in natural disasters around the world. As of now, Disaster Dodger has been programmed to assist in the United States. Simply select your state using the drop down menu on the home page, then you will find the most common natural disasters in your area, and items you can purchase to prepare for them (if one were to happen).
+            </Typography>
+          </div>
+          <div>
+            <Typography variant='h2'>Future Endeavors</Typography>
+            <Typography variant='p'>
+              In order to provide a more personalized system for our users, we wish to implement a profile system where each user with an account can access a dashboard and receive information about their target area. Another idea we had in mind was to directly connect Disaster Dodger with an interactive Amazon store via Google Cloud Product. Lastly, we hope to expand the service to other countries, even other continents, as well. This is just the beginning of an impactful service curated to protect its customers.
+            </Typography>
+          </div>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
